1dc2e5c044b1e8c0821ef36111eaaf199cd3a8f2
John Desrosiers <john.desro@gmail.com>
2021-04-16 15:48:19 -0700

    Add createMovies to db

--- db/index.js
+++ db/index.js
@@ -1,10 +1,30 @@
 const fs = require("fs").promises;
 const path = require("path");
+const { nanoid } = require("nanoid");
 
-// create a function readMovies  which async reads movieDb.json resolves that as a promise
 const readMovies = () =>
   fs
     .readFile(path.join(__dirname, "./movies.json"), "utf-8")
     .then((data) => JSON.parse(data));
 
-module.exports = { readMovies };
+const createMovie = (title, rating) => {
+  if (!title || !rating) {
+    return Promise.reject(new Error("Must have a title and rating"));
+  }
+
+  return readMovies().then((movies) => {
+    const movie = {
+      id: nanoid(),
+      title,
+      rating,
+    };
+    movies.push(movie);
+    return fs.writeFile(
+      path.join(__dirname, "./movies.json"),
+      JSON.stringify(movies),
+      "utf-8"
+    );
+  });
+};
+
+module.exports = { readMovies, createMovie };
